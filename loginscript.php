bb93db83b099f68aae36774d9741c6af0c9f2ff4
sisaan <S7107841@terrace.qld.edu.au>
2017-08-10 12:50:01 +1000

    Create loginscript.php
    needs to be plugged into login screen to function

---
+++ loginscript.php
@@ -0,0 +1,32 @@
+<html>
+<head><title>please do not read me</title></head>
+<?php
+//esttablish variables
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'wairhouse';
+
+//build a bridge to the server
+$connection = mysql_connect($host,$username,$password) or die('problems connecting to the server');
+
+//connect to the server through that bridge
+mysql_select_db($dbname) or die('problems connecting to the server');
+
+//retreive data
+$myquery = 'select email, pass from users order by user';
+
+$answertable = mysql_query($myquery);
+
+if(!$answertable){
+	echo 'no data'; 
+}else {
+	//get the retreived data
+	//echo 'data found';
+	while(list($usr,$pw) = mysql_fetch_row($answertable));
+		echo '<br />'.$usr.',';
+		echo $;
+}
+
+?>
+</html>
