20b63474d687d25fec84f139b922ec80c5865147
centor111 <oscar.tnt@gmail.com>
2017-08-18 09:19:00 +1000

    Update Homepage.php

--- Homepage.php
+++ Homepage.php
@@ -2,6 +2,9 @@
 <html>
   <head>
   <head>
+ 	<link type='text/css' rel='stylesheet' href='style.css'/>
+    <title>WAirHouse</title>
   <body>
+  	<img src="http://http://imgur.com/MJw1ovr.jpg"/>
   </body>
 </html>
