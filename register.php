a073fe0c5c7d2a8cece6f03bf7ed099213821b77
centor111 <oscar.tnt@gmail.com>
2017-08-20 16:46:38 +1000

    Update register.php

--- register.php
+++ register.php
@@ -6,13 +6,13 @@
   	<img src="http://imgur.com/MJw1ovr.jpg"/>
   	<form name="adderer" action="register.php" method="post" id="register">
 	<table>
-		<tr><td>First Name:</td>                    <input type="text" name="fname"><td></td></tr>
-		<tr><td>Surname:</td><td>                   <input type="text" name="Lname"></td></tr>
-		<tr><td>Date of Birth (yyyy-mm-dd):</td><td><input type="date" name="dob"></td></tr>
-		<tr><td>Email:</td><td>                   <input type="text" name="email"></td></tr>
-		<tr><td>Phone Number:</td><td>              <input type="text" name="phnum"></td></tr>
-		<tr><td>Password:</td><td>                   <input type="text" name="Pass"></td></tr>
-		<tr><td>Account type:</td><td>              <select name="typelist" form="register">
+		<tr><td>First Name:</td>                    <input type="text" name="fname" required><td></td></tr>
+		<tr><td>Surname:</td><td>                   <input type="text" name="Lname" required></td></tr>
+		<tr><td>Date of Birth (yyyy-mm-dd):</td><td><input type="date" name="dob" required></td></tr>
+		<tr><td>Email:</td><td>                   <input type="text" name="email" required></td></tr>
+		<tr><td>Phone Number:</td><td>              <input type="text" name="phnum" required></td></tr>
+		<tr><td>Password:</td><td>                   <input type="text" name="Pass" required></td></tr>
+		<tr><td>Account type:</td><td>              <select name="typelist" form="register" required>
  													 <option value="Renter">Renter</option>
 													 <option value="Storer">Storer</option>
 													 <option value="Transporter">Transporter</option>
