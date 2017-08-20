6185767fbb3273ed35cf0591db1e552f1494bae2
centor111 <oscar.tnt@gmail.com>
2017-08-20 15:47:35 +1000

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,14 @@
+<?php
+//set up connections to the databast
+
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_DATABASE", "wairhouse");
+
+//build a bridge to the server
+$link = mysqli_connect("localhost", "root", "")  or die("Unable to connect");
+mysqli_select_db($wairhouse) or die('Problems connecting to database');
+
+?>
+
