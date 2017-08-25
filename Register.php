439ae5bd8651c619ca00d92bd36a77dde3001003
centor111 <oscar.tnt@gmail.com>
2017-08-25 14:00:07 +1000

    Update Register.php

--- Register.php
+++ Register.php
@@ -3,20 +3,22 @@
   <head><title>Register a New Account</title></head>
  	<link type='text/css' rel='stylesheet' href='style.css'/>
   <body>
-  	<img src="http://imgur.com/MJw1ovr.jpg"/><form name="adderer" action="register.php" method="post">
+  	<img src="http://imgur.com/MJw1ovr.jpg"/>
+  	<form name="adderer" action="register.php" method="post" id="register">
 	<table>
-		<tr><td>First Name:</td>                    <input type="text" name="fname"><td></td></tr>
-		<tr><td>Surname:</td><td>                   <input type="text" name="Lname"></td></tr>
-		<tr><td>Date of Birth (yyyy-mm-dd):</td><td><input type="date" name="dob"></td></tr>
-		<tr><td>Email:</td><td>                   <input type="text" name="email"></td></tr>
-		<tr><td>Phone Number:</td><td>              <input type="text" name="phnum"></td></tr>
-		<tr><td>Password:</td><td>                   <input type="text" name="Pass"></td></tr>
-		<tr><td>Account type:</td><td>              <select>
+		<tr><td>First Name:</td>                    <input type="text" name="fname" required><td></td></tr>
+		<tr><td>Surname:</td><td>                   <input type="text" name="Lname" required></td></tr>
+		<tr><td>Date of Birth (yyyy-mm-dd):</td><td><input type="date" name="dob" required></td></tr>
+		<tr><td>Email:</td><td>                   <input type="text" name="email" required></td></tr>
+		<tr><td>Phone Number:</td><td>              <input type="text" name="phnum" required></td></tr>
+		<tr><td>Password:</td><td>                   <input type="text" name="Pass" required></td></tr>
+		<tr><td>Account type:</td><td>              <select name="joe" required>
  													 <option value="Renter">Renter</option>
 													 <option value="Storer">Storer</option>
 													 <option value="Transporter">Transporter</option>
 													</select></td></tr>
-
+		<tr><td>Adress:</td><td>                   <input type="text" name="place" required></td></tr>
+		<tr><td>Adress Description:</td><td>       <input type="text" name="descripacito" required></td></tr>
 		<tr><td>Register:</td><td>                 <input type="submit" name="submit"></td></tr>
 
 	</table>
@@ -33,12 +35,23 @@ if (isset($_POST['submit'])){
 	$c = $_POST['dob'];
 	$d = $_POST['email'];
 	$e = $_POST['phnum'];
-	$f = $_POST['value'];
-	$g = hash('whirlpool',$_POST['Pass'])
+	$f = $_POST['joe'];
+	$g = hash('whirlpool',$_POST['Pass']);
+	$Address = $_POST['place'];
+	$Desc = $_POST['descripacito'];
 //handle photos later
-	$insert = 'insert into friendface(Fname,Lname,Bday,Email,Phnum,Acctype,Pass,Photo)
-			 values ("'.$a.'","."'.$b.'","."'.$c.'","."'.$d.'","."'.$e.'","."'.$f.'","."'.$g.'","JohnDoe.png")';
-	mysql_query($insert,$link);
+	$insert = 'insert into users(Fname,Lname,Bday,Email,Phnum,Acctype,Pass,Photo)
+			 values("'.$a.'","'.$b.'","'.$c.'","'.$d.'","'.$e.'","'.$f.'","'.$g.'","JohnDoe.png")';
+	mysqli_query($link,$insert);
+///No workie zone from here
+	$queryy='select UID from users where Email = "'.$d.'";';
+	$result= mysqli_query($link,$queryy);
+    while($row = mysqli_fetch_assoc($result) {
+    $UsrID= $row['UID'];
+}
+     $locat  = 'insert into location(Lname,LDesc,Owner)
+				values("'.$Address.'","'.$Desc.'",'.$UsrID.')';
+     mysqli_query($link,$locat);
 }
 ?>
 </body>
