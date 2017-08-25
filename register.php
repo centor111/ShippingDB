46ab57a1249632ac5cf86cad6b5539c0f74a8bc6
centor111 <oscar.tnt@gmail.com>
2017-08-25 13:41:15 +1000

    Update register.php

--- register.php
+++ register.php
@@ -12,12 +12,13 @@
 		<tr><td>Email:</td><td>                   <input type="text" name="email" required></td></tr>
 		<tr><td>Phone Number:</td><td>              <input type="text" name="phnum" required></td></tr>
 		<tr><td>Password:</td><td>                   <input type="text" name="Pass" required></td></tr>
-		<tr><td>Account type:</td><td>              <select name="typelist" form="register" required>
+		<tr><td>Account type:</td><td>              <select name="joe" required>
  													 <option value="Renter">Renter</option>
 													 <option value="Storer">Storer</option>
 													 <option value="Transporter">Transporter</option>
 													</select></td></tr>
-
+		<tr><td>Adress:</td><td>                   <input type="text" name="place" required></td></tr>
+		<tr><td>Adress Description:</td><td>       <input type="text" name="descripacito" required></td></tr>
 		<tr><td>Register:</td><td>                 <input type="submit" name="submit"></td></tr>
 
 	</table>
@@ -34,12 +35,28 @@ if (isset($_POST['submit'])){
 	$c = $_POST['dob'];
 	$d = $_POST['email'];
 	$e = $_POST['phnum'];
-	$f = $_POST['typelist'];
+	$f = $_POST['joe'];
 	$g = hash('whirlpool',$_POST['Pass']);
+	$Address = $_POST['place'];
+	$Desc = $_POST['descripacito'];
 //handle photos later
-	$insert = 'insert into friendface(Fname,Lname,Bday,Email,Phnum,Acctype,Pass,Photo)
-			 values ("'.$a.'","."'.$b.'","."'.$c.'","."'.$d.'","."'.$e.'","."'.$f.'","."'.$g.'","JohnDoe.png")';
-	mysqli_query($insert,$link);
+	$insert = 'insert into users(Fname,Lname,Bday,Email,Phnum,Acctype,Pass,Photo)
+			 values("'.$a.'","'.$b.'","'.$c.'","'.$d.'","'.$e.'","'.$f.'","'.$g.'","JohnDoe.png")';
+	mysqli_query($link,$insert);
+///No workie zone from here
+	/* $find = 'select count(*) from users';
+	$newest = (mysqli_query($link,$find)-1);
+	$locat  = 'insert into location(Lname,LDesc,Owner)
+				values("'.$Address.'","'.$Desc.'",'.$newest.')';
+     mysqli_query($link,$locat); */
+	$queryy='select UID from users where Email = '.$d.'';
+	$result= mysqli_query($link,$queryy);
+    while(list($Ussr) = mysqli_fetch_row($result)) {
+    $UsrID= $Ussr;
+}
+     $locat  = 'insert into location(Lname,LDesc,Owner)
+				values("'.$Address.'","'.$Desc.'",'.$UsrID.')';
+     mysqli_query($link,$locat);
 }
 ?>
 </body>
