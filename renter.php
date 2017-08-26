894ce0c81819d35a07b07fcd1a801589bd9c6f53
centor111 <oscar.tnt@gmail.com>
2017-08-26 18:28:17 +1000

    Create renter.php

---
+++ renter.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <head>
+ 	<link type='text/css' rel='stylesheet' href='style.css'/>
+    <title>WAirHouse</title>
+  <body>
+  	<img src="http://imgur.com/MJw1ovr.jpg"/>
+ <?php    
+ 
+ ?>
+  </body>
+</html>
