5ae62fb62112feb4ba706038701c82100cd4ca3a
centor111 <oscar.tnt@gmail.com>
2017-08-28 12:58:43 +1000

    Update renter.php

--- renter.php
+++ renter.php
@@ -6,14 +6,15 @@
     <title>WAirHouse</title>
   <body>
   	<img src="http://imgur.com/MJw1ovr.jpg"/>
- <?php    
- require('connect.php');
- $query = "Select UID,Fname,Lname,Phnum from users where Acctype = 'Storer'";
- $result = mysqli_query($link,$query);  
- $rows = mysqli_num_rows($result); 
- $columns = mysqli_num_fields($result);    
+ <?php  
 
- if ($result->num_rows > 0) {
+ require('connect.php');
+ $summStorer = "Select UID,Fname,Lname,Phnum from users where Acctype = 'Storer'"; //find some storers
+ $rresult = mysqli_query($link,$summStorer);  
+ $rrows = mysqli_num_rows($rresult); 
+ $ccolumns = mysqli_num_fields($rresult);    
+//format the table of users
+ if ($rresult->num_rows > 0) {
     echo "<table border= 1>
       <tr>
         <th>UID</th>
@@ -21,14 +22,38 @@
         <th>Phone number</th>
         </tr>";
     // output data of each row
-    while($row = mysqli_fetch_row()) {
+    while($roww = mysqli_fetch_row()) {
         echo "<tr>
-          <td>".$row["UID"]."</td><td>".$row["Fname"].$row["Lname"]."</td><td>".$row["Phone Number"]."</td>
+          <td>".$roww["UID"]."</td><td>".$row["Fname"].$roww["Lname"]."</td><td>".$roww["Phone Number"]."</td>
               </tr>";
     }
     echo "</table>";
 } else {
-    echo "There are no Storers currently catered to your Item :(";
+    echo "There are no Storers currently catered to your Item :("; 
+  }
+
+//Items table
+  $itemm = "Select * from Items where Owner = $user";  //Find the items the user owns
+ $result = mysqli_query($link,$itemm);  
+ $rows = mysqli_num_rows($result);   
+ $columns = mysqli_num_fields($result);    
+ if ($result->num_rows > 0) {
+    echo "<table border= 1>
+      <tr>
+        <th>Item</th>
+        <th>Storer</th>
+        <th>Location</th>
+        </tr>";
+    // output data of each row
+    while($row = mysqli_fetch_row()) {
+        echo "<tr>
+          <td>".$row["IName"]."</td><td>".$row["Storer"]."</td><td>".$row["Location"]."</td>
+              </tr>";
+    }
+    echo "</table>";
+} else {
+    echo "You have no registered items :(";
+}
  ?>
   </body>
 </html>
