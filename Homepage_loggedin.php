dda83b851aa0965df29e3b1d75643eb8f070d0a2
sisaan <S7107841@terrace.qld.edu.au>
2017-08-28 14:52:17 +1000

    Update Homepage_loggedin.php

--- Homepage_loggedin.php
+++ Homepage_loggedin.php
@@ -4,64 +4,35 @@
    </head>
   <body>
     <img src="http://imgur.com/MJw1ovr.jpg"/>
-  <?PHP
-  switch (select acctype from users where UID = $currentuser) {
-    case 0:
-      ?>
-  	
-  	<form name="regitem" action="register.php" method="post" id="register">
-	<table>
-		<tr><td>Item Name:</td>                    <input type="text" name="iname" required><td></td></tr>
-		<tr><td>Item Description:</td><td>                   <input type="text" name="Idesc" required></td></tr> 													
-		<tr><td>Submit item:</td><td>                 <input type="submit" name="submit"></td></tr>
-
-	</table>
-</form>
-
-</body>
-<?php
-if (isset($_POST['submit'])){
-	//stuff to add to the table
-	require('connect.php');
-	$a = $_POST['Iname'];
-	$b = $_POST['Idesc'];
-	$c = $_Session['Currentuser'];
-	$d = 0;
-	$e = $_Session['CurrentLocation'];
-	$insert = 'insert into Items(IName, IDesc, Owner, Storer, IStatus, ILocation)
-			 values("'.$a.'","'.$b.'","'.$c.'","'.$c.'","'.$d.'","'.$e.'")';
-	mysqli_query($link,$insert);
-///find the user being registered
-	$queryy='select UID from users where Email = "'.$d.'";';
-	$result= mysqli_query($link,$queryy);
-    while($row = mysqli_fetch_row($result)) {
-    $UsrID = $row[0] ;
-     $locata  = 'insert into location(Lname,LDesc,Owner)
-				values("'.$Address.'","'.$Desc.'",'.$UsrID.')';
-     mysqli_query($link,$locata);
- }
-}
-?>
-    
-    
-    
-    <table>
-    <TR>
-        Require(iteminsert.php);
-        $myitems = 'Select IID, IName, IDesc, Storer, ILocation from items where ((owner = '.$currentuser.') and (IStatus = 1)'
-        Echo 'stored items:'
-        mysqli_query($link,$myitems);
-        $nyitems = 'Select IID, IName, IDesc, Storer, ILocation from items where ((owner = '.$currentuser.') and (IStatus = 0)'
-        Echo 'current ads'
-        mysqli_query($link,$nyitems);
-        
+	  <form action="Homepage_loggedin.php" method="post">
+ 		<input type="submit" action="post" name="submit" value="Log out"> 
+	  </form>
+  <?php
+	  if (isset($_POST['submit'])){
+		  session_unset(); 
+		session_destroy();
+	  } else {
+  switch (mysql_query($link,'select acctype from users where UID = '.$_SESSION['currentuser'].'') {
+    case "renter":
+      ?> 
+      <form action="Homepage_loggedin.php" method="get">
+ <input type="submit" formaction="fetch.php" value="my items"> 
+ <input type="submit" formaction="iteminsert.php" value="create new ad">
+ <input type="submit" formaction="Renter.php" value="store an item"> 
+ <input type="submit" formaction="unstore.php" value="Submit to Page3">
+    </form> 
+    <?php
         break;
-    case 1:
-        Require(storer.php);
+    case "storer":
+	  ?>
+       <form action="Homepage_loggedin.php" method="get">
+ <input type="submit" formaction="storefetch.php" value="my items"> 
+	</form>
+	  <?php
         break;
-    case 2:
-        Require(transporter.php);
+    case "transporter":
+        Echo "Work in Progress";
         break;
-}
+}}
   ?>
 </html>
