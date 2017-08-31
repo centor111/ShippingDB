c92f319cd35efbc24223b462de05168eb0eac97b
centor111 <oscar.tnt@gmail.com>
2017-09-01 00:34:03 +1000

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,14 @@
   <head><title>Welcome</title></head>
  	<link type='text/css' rel='stylesheet' href='style.css'/>
   <body>
+  	<script src="dotty.js"></script>
   	<img src="http://imgur.com/MJw1ovr.jpg" align="middle"/>
-  	<table width="100" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+  	<table width="100" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#89c8e5">
 <tr> <form name="welcome" method="post" action="index.php">  
-<td> <table width="100%" border="0" cellpadding="10" cellspacing="10" bgcolor="#FFFFFF">
+<td> <table width="100%" border="0" cellpadding="10" cellspacing="10" bgcolor="#89c8e5">
 <tr><td><h2>Welcome to WairHouse!</h2></td></tr>
-<tr> <td width="100"><input type='submit' name="Register" value='Register'></td> </tr>
-<tr> <td><input name="Login" type="submit" value="Login"></td></tr>
+<tr> <td width="100"><input formaction="register.php" type='submit' value='Register'></td> </tr>
+<tr> <td><input formaction="login.php" type="submit" value="Login"></td></tr>
 </table></td></form></tr></table>
 </body>
 </html>
