19f6f3421a82d26d933f0ee0de57dcc9f0141c10
sisaan <S7107841@terrace.qld.edu.au>
2017-08-31 11:47:11 +1000

    Update ItemInsert.php

--- ItemInsert.php
+++ ItemInsert.php
@@ -3,6 +3,9 @@
   <head><title>Register a Item</title></head>
  	<link type='text/css' rel='stylesheet' href='style.css'/>
   <body>
+	  <form action="Homepage_loggedin.php" method="get">
+		 <input type="submit" formaction="Homepage_loggedin.php" value="my items">
+	  </form>
   	<img src="http://imgur.com/MJw1ovr.jpg"/><form name="adderer" action="register.php" method="post">
 	<table>
 		<tr><td>Item Name:</td>                    <input type="text" name="itmname"><td></td></tr>
