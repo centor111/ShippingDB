e5db44747b1707055d931f0e765cf9991a9607a1
centor111 <oscar.tnt@gmail.com>
2017-09-01 00:13:02 +1000

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head><title>Welcome</title></head>
+ 	<link type='text/css' rel='stylesheet' href='style.css'/>
+  <body>
+  	<img src="http://imgur.com/MJw1ovr.jpg" align="middle"/>
+  	<table width="100" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+<tr> <form name="welcome" method="post" action="index.php">  
+<td> <table width="100%" border="0" cellpadding="10" cellspacing="10" bgcolor="#FFFFFF">
+<tr><td><h2>Welcome to WairHouse!</h2></td></tr>
+<tr> <td width="100"><input type='submit' name="Register" value='Register'></td> </tr>
+<tr> <td><input name="Login" type="submit" value="Login"></td></tr>
+</table></td></form></tr></table>
+</body>
+</html>
