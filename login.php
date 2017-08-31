f2a7154bf90b1c5486f22a2707b5a10008fe4892
centor111 <oscar.tnt@gmail.com>
2017-08-31 15:56:16 +1000

    Create login.php

---
+++ login.php
@@ -0,0 +1,46 @@
+  <?php session_start() ?>
+<!DOCTYPE html>
+<html>
+  <head><title>Login</title></head>
+ 	<link type='text/css' rel='stylesheet' href='style.css'/>
+  <body>
+  	<img src="http://imgur.com/MJw1ovr.jpg" align="middle"/>
+  	<table width="300" border="0" align="center" cellpadding="0" cellspacing="1" bgcolor="#CCCCCC">
+<tr> <form name="login" method="post" action="login.php">  <td>
+<table width="100%" border="0" cellpadding="3" cellspacing="1" bgcolor="#FFFFFF">
+<tr> <td colspan="3"><strong>Member Login </strong></td> </tr>
+<tr> <td width="84">Email:</td>
+<td width="294"><input name="myusername" type="text" id="myusername"></td> </tr>
+<tr> <td>Password:</td>
+<td><input name="mypassword" type="text" id="mypassword"></td></tr>
+<tr> <td>&nbsp;</td>
+<td>&nbsp;</td>
+<td><input type="submit" name="submit" value="Login"></td> </tr>
+</table></td></form></tr></table>
+
+<?php
+if (isset($_POST['submit'])){
+	require('connect.php');
+	$username = $_POST['myusername'];  //harvest
+	$password = $_POST['mypassword'];
+	$password = hash('whirlpool', $password);
+	$sql ="SELECT Email, Pass, UID from users where Email = '$username' and pass = '$password'";
+	$result=mysqli_query($link,$sql);
+
+	$count=mysql_num_rows($result); //counting table rows
+
+	if($count==1){ // If result matched $myusername and $mypassword, table row must be 1 row
+	$queryy='select UID from users where Email = "$username"';  ///find the accociated UID
+	$result= mysqli_query($link,$queryy);
+    while($row = mysqli_fetch_row($result)) {
+    $UsrID = $row[0] ;
+	$_SESSION['currentuser'] = $UsrID; //store to session
+	echo '<br><a href="Homepage_loggedin.php"></a>'; //redirect to homepage
+	}
+	}else {
+	echo "Wrong Username or Password";
+			}
+}
+?>
+</body>
+</html>
