2e69590a5d5818210e1480ae791ce15b845302a4
centor111 <oscar.tnt@gmail.com>
2017-08-31 21:27:27 +1000

    Update Homepage_loggedin.php

--- Homepage_loggedin.php
+++ Homepage_loggedin.php
@@ -8,36 +8,37 @@
  		<input type="submit" action="post" name="submit" value="Log out"> 
 	  </form>
   <?php
-	  if (isset($_POST['submit'])){
+  require('connect.php');
+	  if (isset($_POST['submit'])){ //end the session when this button is clicked
 		  session_unset(); 
 		  session_destroy();
       echo('Logged Out.');
 	  } else {
-      $user = $_SESSION['currentuser'];
-      $test ='select acctype from users where UID = $user';
-      $result = mysql_query($link, $test);
+      $user =$_SESSION['currentuser']; //find out who the current user is
+      $test ='SELECT acctype from users where UID = '.$user.'';
+      $result = mysqli_query($link, $test);
       while($row = mysqli_fetch_row($result)) {
-      $testquery = $row[0] ;
+      $testquery = $row[0] ; //plate up
       }
   switch ($testquery) {
-    case "renter":
+    case "renter": //if the current user is a renter
       ?> 
       <form action="Homepage_loggedin.php" method="get">
       <input type="submit" formaction="fetch.php" value="my items"> 
       <input type="submit" formaction="iteminsert.php" value="create new ad">
       <input type="submit" formaction="Renter.php" value="store an item"> 
-      <input type="submit" formaction="unstore.php" value="Submit to Page3">
+      <input type="submit" formaction="unstore.php" value="take an item out of storage">
       </form> 
       <?php
         break;
-    case "storer":
+    case "storer": //if the current user is a storer
 	    ?>
        <form action="Homepage_loggedin.php" method="get">
        <input type="submit" formaction="storefetch.php" value="my items"> 
 	     </form>
 	    <?php
         break;
-    case "transporter":
+    case "transporter": //if the current user is a transporter. (Not implemented)
         Echo "Work in Progress";
         break;
       }
