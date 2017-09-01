fac223abca83cce3ac325138c1705cc45e883014
centor111 <oscar.tnt@gmail.com>
2017-09-01 21:00:32 +1000

    Update ItemInsert.php

--- ItemInsert.php
+++ ItemInsert.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
+<?php session_start() ?>
 <html>
   <head><title>Register a Item</title></head>
  	<link type='text/css' rel='stylesheet' href='style.css'/>
   <body>
 	  <img src="http://imgur.com/MJw1ovr.jpg"/>
-	  <form action="Homepage_loggedin.php" method="get">
-		 <input type="submit" formaction="Homepage_loggedin.php" value="home">
-	  </form>
+ 
+	  <form name="newitem" action="iteminsert.php" method="post" id="newitem">
 	<table>
 		<tr><td>Item Name:</td>                    <input type="text" name="itmname"><td></td></tr>
 		<tr><td>Description:</td><td>                   <input type="text" name="desc"></td></tr>
@@ -20,17 +20,20 @@ if (isset($_POST['submit'])){
 	//stuff to add to the table
 	require('connect.php');
 	$a = $_POST['itmname'];
-	$b = 'live';             //0 is storage required, 1 is currently stored, 2 is retreived
-  $c = $_SESSION['CurrentUser'];
-	$d = $_SESSION['CurrentUser'];              //no storer on creation, set it to the owner (renter)
-	$e = $_SESSION['CurrentLocation'];              //set the location of the item to the renter's home
+	$b = 'live';             //need to change these to numbers...
+  $c = $_SESSION['currentuser'];
+	$d = $_SESSION['currentuser'];              //no storer on creation, set it to the owner (renter)
+	$e = $_SESSION['currentlocation'];              //set the location of the item to the renter's home
 	$f = $_POST['desc'];
 //handle photos later
 	$insert = 'insert into items(IName,IStatus,Owner,Storer,Location,IDesc)
-			 values ("'.$a.'","'.$b.'","'.$c.'","'.$d.'","'.$e.'","'.$f.'")';
-	mysql_query($link,$insert);
-	header("Location: http://localhost/wairhouse/Homepage_loggedin.php"); 
+			 values ("'.$a.'","'.$b.'",'.$c.','.$d.','.$e.',"'.$f.'")';
+	mysqli_query($link,$insert);
+	echo "Successfully Added!";
 }
 ?>
+		<form name="return" action="iteminsert.php" method="post" id="return">
+		<input type="submit" formaction="Homepage_loggedin.php" value="Return"> 
+</form>
 </body>
 </html>
