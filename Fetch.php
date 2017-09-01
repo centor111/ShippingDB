fc6a9de5597f5203c92f827c2d797cb3660d686e
centor111 <oscar.tnt@gmail.com>
2017-09-01 20:21:51 +1000

    Update Fetch.php

--- Fetch.php
+++ Fetch.php
@@ -1,27 +1,24 @@
 <!DOCTYPE html>
-<?php session_start() ?>
+<?php session_start() //start the session ?>
 <html>
-	<head><title>My Items</title> <head>
+  <head><title>My Current Tasks</title><head>
   <link type='text/css' rel='stylesheet' href='style.css'/>
     <title>WAirHouse</title>
   <body>
     <img src="http://imgur.com/MJw1ovr.jpg"/>
     <form action="Homepage_loggedin.php" method="get">
-		 <input type="submit" formaction="Homepage_loggedin.php" value="my items">
-	  </form>
- <?php 
+     <input type="submit" formaction="Homepage_loggedin.php" value="home">
+    </form>
+ <?php  
  $user = $_SESSION['currentuser'];
- //echo($user);
- //phpinfo();
  //query engine 
  require('connect.php');
- print "The Items you currently have registered:";
- $query = "Select * from Items where Owner = ".$user."";  //Find the items the user owns
+ print "The Items you own: ";
+ $query = "Select * from Items where owner = $user";  //Find the items stored by the user
  $result = mysqli_query($link,$query);  
  $rows = mysqli_num_rows($result); 
  //print "There are  $rows results<p>";  
  $columns = mysqli_num_fields($result);    
-
  if ($result->num_rows > 0) {
     echo "<table border= 1>
       <tr>
@@ -32,12 +29,12 @@
     // output data of each row
     while($row = mysqli_fetch_row()) {
         echo "<tr>
-          <td>".$row["IName"]."</td><td>".$row["Storer"]."</td><td>".$row["Location"]."</td>
+          <td>".$row["IName"]."</td><td>".$row["Owner"]."</td><td>".$row["Location"]."</td>
               </tr>";
     }
     echo "</table>";
 } else {
-    echo "You have no registered items :(";
+    echo " You have no items. registe one to get started and join the fun!";
 }
  ?>
   </body>
